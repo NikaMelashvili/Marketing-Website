89dfa82ed2e77ef693a2d3cf79a53ce299eab760
Nika Melashvili <109832550+NikaMelashvili@users.noreply.github.com>
2024-01-02 00:16:23 +0400

    scrolling smooth
    smooth scrolling implemented.

--- MarketingSiteForBankist/script.js
+++ MarketingSiteForBankist/script.js
@@ -3,11 +3,16 @@
 ///////////////////////////////////////
 // Modal window
 
+//Query elements
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+const scrollBtn = document.querySelector('.btn--scroll-to');
+const firstElement = document.getElementById('section--1');
 
+//Functions
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -19,6 +24,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
+//Event listeners
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
@@ -28,4 +34,26 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+const cookieMessage = document.createElement('div');
+cookieMessage.classList.add('cookie-message');
+cookieMessage.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+header.prepend(cookieMessage);
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  cookieMessage.remove();
+});
+
+scrollBtn.addEventListener('click', () => {
+  //this is for old browsers
+  // const s1cords = firstElement.getBoundingClientRect();
+  // window.scrollTo({
+  //   left: s1cords.left + window.pageXOffset,
+  //   top: s1cords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  firstElement.scrollIntoView({ behavior: 'smooth' });
 });
